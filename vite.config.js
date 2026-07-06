d47a2bf4bbae1ff431f379a5f07d01edcb031a9b
Doaaite <takritidoaa@gmail.com>
2026-07-06 11:31:27 +0300

    push

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-    base: '/dental-clinic-app' // مثلاً: '/dental-clinic-app'
+     base: './', // أو '/'
+  server: {
+    historyApiFallback: true
+  }
 })
